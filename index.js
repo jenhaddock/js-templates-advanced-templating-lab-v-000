3c1fa8439a46bcf8eebc38ddac130eb6ce6b5ed8
Jennifer Haddock <jentx123@hotmail.com>
2017-11-26 23:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  let form = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML)
+
+  document.getElementsByTagName('main')[0].innerHTML = form({'submitAction': 'createRecipe()'})
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+  init() 
 })
